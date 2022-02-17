ac1db72579b17f7f8170b5f71676ed58183f2a7e
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-17 12:44:30 -0800

    set up first migration

--- data/migrations/20220217200045_recipe-book.js
+++ data/migrations/20220217200045_recipe-book.js
@@ -1,8 +1,54 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments('recipe_id');
+      tbl.string('recipe_name', 128)
+        .unique()
+        .notNullable();
+      tbl.timestamp('created_at', { useTz: true });
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments('ingredient_id');
+      tbl.string('ingredient_name')
+        .unique()
+        .notNullable()
+    })
+    .createTable('steps', tbl => {
+      tbl.increments('step_id');
+      tbl.integer('step_number')
+        .unsigned()
+        .notNullable();
+      tbl.string('step_instructions')
+        .notNullable();
+      tbl.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      tbl.string('ingredient_name')
+        .notNullable()
+        .references('ingredient_name')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      tbl.decimal('ingredient_quantity')
+        .notNullable();
+      tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT');
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
 };
